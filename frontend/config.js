13d236fac432fae3a7b5383bf38bb4169c1a1ae8
Krishnagokul1305 <krishnagokul1729@gmail.com>
2024-08-04 17:14:56 +0530

    cleared all the consoles in backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,8 +1,6 @@
-export const API_BASE_URL = "http://127.0.0.1:8000/api/v1";
+export const API_BASE_URL = "https://natours-app-t4vm.onrender.com/api/v1";
 export const TOUR_COVER = `${API_BASE_URL}/public/img/toursCover`;
-export const TOUR_IMAGES =
-  `${API_BASE_URL}/public/img/toursImages`;
+export const TOUR_IMAGES = `${API_BASE_URL}/public/img/toursImages`;
 export const USER_IMG = `${API_BASE_URL}/public/img/user`;
-
 
 // https://natours-app-t4vm.onrender.com
