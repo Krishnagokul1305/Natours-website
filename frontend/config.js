6f7678b4216fb8fd64bc70848649c7bd02f0a90e
Krishnagokul1305 <krishnagokul1729@gmail.com>
2024-08-04 14:19:13 +0530

    public folder for images created

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,7 @@
-export const API_BASE_URL = "http://127.0.0.1:8000/api/v1";
+export const API_BASE_URL = "https://natours-app-t4vm.onrender.com/api/v1";
 export const TOUR_COVER = "http://127.0.0.1:8000/api/v1/public/img/toursCover";
 export const TOUR_IMAGES =
   "http://127.0.0.1:8000/api/v1/public/img/toursImages";
 export const USER_IMG = "http://127.0.0.1:8000/api/v1/public/img/user";
+
+// https://natours-app-t4vm.onrender.com
